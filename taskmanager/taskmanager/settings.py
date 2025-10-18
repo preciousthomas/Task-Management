fd44ba522a522eb55cd021fa04cc7b3d13163de4
precious thomas <preshusthomas28@gmail.com>
2025-10-18 16:30:45 +0100

    task manager

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',  
-    'tasks',            
+    'rest_framework',
+    'rest_framework_simplejwt',  # Add JWT auth
+    'tasks',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+}
--- taskmanager/tasks/views.py
+++ taskmanager/tasks/views.py
@@ -30,3 +30,34 @@ class TaskViewSet(viewsets.ModelViewSet):
         task.completed = False
         task.save()
         return Response({'status': 'Task marked as incomplete'})
+    from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Task
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # ✅ Only return tasks belonging to the authenticated user
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        # ✅ Automatically assign the logged-in user to new tasks
+        serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def mark_complete(self, request, pk=None):
+        task = self.get_object()
+        task.completed = True
+        task.save()
+        return Response({'status': 'Task marked as complete'})
+
+    @action(detail=True, methods=['post'])
+    def mark_incomplete(self, request, pk=None):
+        task = self.get_object()
+        task.completed = False
+        task.save()
+        return Response({'status': 'Task marked as incomplete'})
